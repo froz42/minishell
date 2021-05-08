133fdeecbc4adf0fdab6beb39314e5f029cba87c
tmatis <tmatis@student.42.fr>
2021-05-08 14:59:51 +0200

    Fix redir when open error

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tmatis <tmatis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 10:44:38 by tmatis            #+#    #+#             */
-/*   Updated: 2021/05/08 14:52:32 by tmatis           ###   ########.fr       */
+/*   Updated: 2021/05/08 14:58:24 by tmatis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -107,28 +107,32 @@ int		 handle_buildin(t_list *commands_list, t_list **env_var)
 	if (ft_lstsize(commands_list) == 1)
 	{
 		command = *(t_command *)commands_list->content;
-		redirect_fd(command, fd_backup);
-		argv = build_argv(command.name, command.args);
-		argc = build_argc(argv);
-		if (ft_strcmp(command.name, "cd") == 0)
-			ret = return_value_buildin(ft_cd(argc, argv, env_var), env_var);
-		else if (ft_strcmp(command.name, "echo") == 0)
-			ret = return_value_buildin(ft_echo(argc, argv), env_var);
-		else if (ft_strcmp(command.name, "env") == 0)
-			ret = return_value_buildin(ft_env(*env_var), env_var);
-		else if (ft_strcmp(command.name, "unset") == 0)
-			ret = return_value_buildin(ft_unset(argc, argv, env_var), env_var);
-		else if (ft_strcmp(command.name, "pwd") == 0)
-			ret = return_value_buildin(ft_pwd(), env_var);
-		else if (ft_strcmp(command.name, "export") == 0)
-			ret = return_value_buildin(ft_export(argc, argv, env_var), env_var);
-		else if (ft_strcmp(command.name, "exit") == 0)
-			ret = ft_exit(argc, argv, env_var, true) + 2;
-		free_table(&argv);
-		if (ft_lstsize(command.redirs) > 0)
+		if (redirect_fd(command, fd_backup))
+			ret = return_value_buildin(1, env_var);
+		else
 		{
-			dup2(fd_backup[0], STDIN_FILENO);
-			dup2(fd_backup[1], STDOUT_FILENO);
+			argv = build_argv(command.name, command.args);
+			argc = build_argc(argv);
+			if (ft_strcmp(command.name, "cd") == 0)
+				ret = return_value_buildin(ft_cd(argc, argv, env_var), env_var);
+			else if (ft_strcmp(command.name, "echo") == 0)
+				ret = return_value_buildin(ft_echo(argc, argv), env_var);
+			else if (ft_strcmp(command.name, "env") == 0)
+				ret = return_value_buildin(ft_env(*env_var), env_var);
+			else if (ft_strcmp(command.name, "unset") == 0)
+				ret = return_value_buildin(ft_unset(argc, argv, env_var), env_var);
+			else if (ft_strcmp(command.name, "pwd") == 0)
+				ret = return_value_buildin(ft_pwd(), env_var);
+			else if (ft_strcmp(command.name, "export") == 0)
+				ret = return_value_buildin(ft_export(argc, argv, env_var), env_var);
+			else if (ft_strcmp(command.name, "exit") == 0)
+				ret = ft_exit(argc, argv, env_var, true) + 2;
+			free_table(&argv);
+			if (ft_lstsize(command.redirs) > 0)
+			{
+				dup2(fd_backup[0], STDIN_FILENO);
+				dup2(fd_backup[1], STDOUT_FILENO);
+			}
 		}
 	}
 	return (ret);
