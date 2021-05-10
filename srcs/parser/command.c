9ac7dfc0fedf8173f8d02370afdc68e9ff7c323b
tmatis <tmatis@student.42.fr>
2021-05-10 16:51:38 +0200

    add --color option for ls

--- srcs/parser/command.c
+++ srcs/parser/command.c
@@ -6,7 +6,7 @@
 /*   By: tmatis <tmatis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/15 15:28:01 by tmatis            #+#    #+#             */
-/*   Updated: 2021/05/09 20:52:59 by tmatis           ###   ########.fr       */
+/*   Updated: 2021/05/10 16:51:18 by tmatis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -174,5 +174,7 @@ t_command	*get_command(t_list **word_list, t_list *env_var)
 		else
 			ft_lstadd_back(&command->args, ft_lstnew(get_arg(word_list)));
 	}
+	if (!ft_strcmp(command->name, "ls"))
+		ft_lstadd_back(&command->args, ft_lstnew(ft_strdup("--color")));
 	return (command);
 }
