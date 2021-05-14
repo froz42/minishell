babd0015d30f868b3cd3b665b8cb68c4909c811a
tmatis <tmatis@student.42.fr>
2021-05-14 17:55:13 +0200

    fix issue19

--- srcs/buildin/export.c
+++ srcs/buildin/export.c
@@ -6,7 +6,7 @@
 /*   By: tmatis <tmatis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/07 15:40:14 by tmatis            #+#    #+#             */
-/*   Updated: 2021/05/14 09:56:53 by tmatis           ###   ########.fr       */
+/*   Updated: 2021/05/14 17:54:35 by tmatis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -112,7 +112,7 @@ void	add_var(char *key, char *work_str, t_list **env_var, int append)
 	int		len_var;
 
 	len_var = 0;
-	if (!*work_str)
+	if (!*work_str && *(work_str - 1) != '=')
 		edit_var(env_var, key, NULL);
 	else
 	{
