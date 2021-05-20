bfb851d96d94bec78505c90da155c9d160854ff7
tmatis <tmatis@student.42.fr>
2021-05-20 22:25:41 +0200

    Style

--- srcs/exec/exec.h
+++ srcs/exec/exec.h
@@ -6,7 +6,7 @@
 /*   By: tmatis <tmatis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 14:12:28 by tmatis            #+#    #+#             */
-/*   Updated: 2021/05/19 14:21:47 by tmatis           ###   ########.fr       */
+/*   Updated: 2021/05/20 22:21:51 by tmatis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,6 @@ int	exec(t_list *pipes_list, t_list **env_var);
 void execution_error_write(char *cmd, char *error);
 int				redirect_fd(t_command command, int backup[2]);
 void	file_error(char *file, char *error);
+void 	restore_in_out(int backup[2]);
 
 #endif
