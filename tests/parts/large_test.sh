6185fc11c5601173606fb54367119ec8c8bef84b
tmatis <tmatis@student.42.fr>
2021-05-21 15:47:51 +0200

    add test

--- tests/parts/large_test.sh
+++ tests/parts/large_test.sh
@@ -11,4 +11,21 @@ echo bonjour \; ls
 echo hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello
 export | grep USER
 env | grep USER
+/bin/echo "hello    wooorld"
+/bin/echo "hel|lo   ;wooorld;"
+/bin/echo "h\el\|lo   \;woo\orld;"
+/bin/echo "'yoooo'"
+/bin/echo "'yoooo\'"
+/bin/echo "'yoooo tok\en\5'\""
+/bin/echo "one      big     token"
+/bin/echo "one      big     token""hello"
+/bin/echo hi"one      big     token"hello
+/bin/echo hello;
+/bin/echo Bonjouuuur; /bin/echo yoooo
+/bin/echo Bonjouuuur; /bin/cat yoooo ; /bin/echo Bonjouuuur
+echo $?
+/bin/echo Bonjouuuur  ;/bin/cat yoooo
+echo $?
+echo hello | wc -l
+export | grep USER | wc -l
 exit 0
