3c182ea4751a264392b61d4c8d40426a63f0830b
tmatis <tmatis@student.42.fr>
2021-05-25 12:12:08 +0200

    fix unset path

--- srcs/exec/execution_rules.c
+++ srcs/exec/execution_rules.c
@@ -6,7 +6,7 @@
 /*   By: tmatis <tmatis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/22 19:24:22 by tmatis            #+#    #+#             */
-/*   Updated: 2021/05/24 14:33:23 by tmatis           ###   ########.fr       */
+/*   Updated: 2021/05/25 12:10:46 by tmatis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static t_bool	is_directory(const char *path)
 	struct stat	statbuf;
 
 	if (stat(path, &statbuf) != 0)
-		return (true);
+		return (false);
 	return (S_ISDIR(statbuf.st_mode));
 }
 
@@ -60,6 +60,7 @@ static int	rules(t_command command, char **argv,
 	{
 		if (is_directory(command.cmd))
 		{
+			printf("cmd is: %s\n", command.cmd);
 			execution_error_write(command.name, "Is a directory");
 			return_value = 126 + 2;
 		}
