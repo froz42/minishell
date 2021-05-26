db3bf3a83166a4150a0d803ad24cc07dc36e85ed
tmatis <tmatis@student.42.fr>
2021-05-26 17:51:00 +0200

    even better

--- srcs/parser/dyn_parsing.c
+++ srcs/parser/dyn_parsing.c
@@ -6,7 +6,7 @@
 /*   By: tmatis <tmatis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 14:12:28 by tmatis            #+#    #+#             */
-/*   Updated: 2021/05/26 17:48:28 by tmatis           ###   ########.fr       */
+/*   Updated: 2021/05/26 17:50:31 by tmatis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ t_list	*get_next_pipes(char **str, int *error, t_list **env_var)
 	t_list	*pipes_list;
 
 	token_list = tokenize(str, error, *env_var, true);
-	if (!token_list && *error != NO_ERROR)
+	if (*error != NO_ERROR)
 	{
 		ft_log_error(strerror(errno));
 		set_status_env(env_var, 2);
